fee2c5029c99f71dbaa99e8913360803a77ad5a5
sherwin-77 <sherwinak1@gmail.com>
2024-04-01 12:46:23 +0700

    Sync with origin commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:another_flutter_splash_screen/another_flutter_splash_screen.dart';
+import 'package:cakra_asset_management/src/pages/landing_page/index.dart';
 import 'package:flutter/material.dart';
 import 'package:cakra_asset_management/theme/theme_provider.dart';
 import 'package:provider/provider.dart';
@@ -16,46 +18,30 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const EmptyPage(),
-      theme: Provider.of<ThemeProvider>(context).themeData,
-    );
-  }
-}
-
-class EmptyPage extends StatelessWidget {
-  const EmptyPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Empty Page'),
+    return FlutterSplashScreen.fadeIn(
+      gradient: const LinearGradient(
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [
+          Color.fromRGBO(24, 0, 119, 1),
+          Color.fromRGBO(54, 0, 0, 1),
+        ],
       ),
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              child: const Text('Cakra Software Developtment'),
-            ),
-            ListTile(
-              title: const Text('Change Theme'),
-              onTap: () {
-                Provider.of<ThemeProvider>(context, listen: false).toggleTheme();
-                Navigator.pop(context); // Close the drawer after toggling theme
-              },
-            ),
-          ],
-        ),
+      onInit: () {
+        debugPrint("On Init");
+      },
+      onEnd: () {
+        debugPrint("On End");
+      },
+      childWidget: SizedBox(
+        height: 250,
+        width: 250,
+        child: Image.asset("assets/logo-white.png"),
       ),
-      body: const Center(
-        //everything goes here
-      ),
+      onAnimationEnd: () {
+        debugPrint("On Fade In End");
+      },
+      nextScreen: const LandingPage(),
     );
   }
 }
